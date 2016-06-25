8e56a0499a66946ce44b84a99cd46626b7cb04f4
Matthew <matthew.thompson.a@gmail.com>
2016-06-25 18:25:58 -0400

    updated SPA with more content css and ng-click controllers.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,8 +2,11 @@ app.controller('HomeController', function($scope) {
   $scope.message = 'Hello from HomeController';
 });
 
-app.controller('ContactController', function($scope) {
+app.controller('ContactController', function($scope, $window) {
   $scope.message = 'Hello from ContactController';
+  $scope.angularAlert = function() {
+    $window.alert('If only I had made a form...');
+  };
 });
 
 app.controller('AboutController', function($scope) {
